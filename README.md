8244fab28e2a5b13284365754efd406cf2ffa193
Jeffrey Ratcliffe <4869402+carygravel@users.noreply.github.com>
2022-05-12 00:00:00 +0000

    Add synopsis and some description

--- README.md
+++ README.md
@@ -8,7 +8,29 @@ Perl2Python - Tool to transcribe Perl 5 to Python 3
 
 # SYNOPSIS
 
+This module attempts to transcribe Perl 5 code to Python 3. This will not be
+successful for all but the simplest code, but it should do most of the legwork.
+
 # DESCRIPTION
+
+You will probably be left with the following problems to solve:
+
+- Python is much stricter about types than Perl. Perl will happily convert a
+string with a number to a number for comparison purposes. Python requires this
+to be explicit, so you may well have to add casts.
+- Similarly, in Perl, you can create a scalar and turn it into a hashref or an
+arrayref by using hash or array functions on it. In Python, you have to
+explicitly initialise the dict or list first, so these statements will have to
+be added.
+- Perl has a very loose concept of instance variables in classes. In Python, these
+must previously declared.
+- Perl does not distiguish between instance variables in classes and hash keys.
+perl2python makes a guess that any keys for objects called "self" are instance
+variables, and anything else are dict keys. This will often be wrong.
+- All Perl modules that are in the list of directory to be searched are
+automatically identified. Python modules are identified partly by their path,
+and thus either the import statements or their calls will often have to be
+adjusted.
 
 # SUBROUTINES/METHODS
 
--- lib/Perl2Python.pm
+++ lib/Perl2Python.pm
@@ -3108,7 +3108,42 @@ Perl2Python - Tool to transcribe Perl 5 to Python 3
 
 =head1 SYNOPSIS
 
+This module attempts to transcribe Perl 5 code to Python 3. This will not be
+successful for all but the simplest code, but it should do most of the legwork.
+
 =head1 DESCRIPTION
+
+You will probably be left with the following problems to solve:
+
+=over
+
+=item *
+Python is much stricter about types than Perl. Perl will happily convert a
+string with a number to a number for comparison purposes. Python requires this
+to be explicit, so you may well have to add casts.
+
+=item *
+Similarly, in Perl, you can create a scalar and turn it into a hashref or an
+arrayref by using hash or array functions on it. In Python, you have to
+explicitly initialise the dict or list first, so these statements will have to
+be added.
+
+=item *
+Perl has a very loose concept of instance variables in classes. In Python, these
+must previously declared.
+
+=item *
+Perl does not distiguish between instance variables in classes and hash keys.
+perl2python makes a guess that any keys for objects called "self" are instance
+variables, and anything else are dict keys. This will often be wrong.
+
+=item *
+All Perl modules that are in the list of directory to be searched are
+automatically identified. Python modules are identified partly by their path,
+and thus either the import statements or their calls will often have to be
+adjusted.
+
+=back
 
 =for readme stop
 
